24ac4138d42e076a9fe57ce421c197070aba14b6
programmerinprogress <msharp23@hotmail.co.uk>
2020-01-04 13:49:23 +0000

    After a 2-day mental block, I got a really...really basic test for DrawMap working.

--- Scripts/mapwork.rendermanager.test.js
+++ Scripts/mapwork.rendermanager.test.js
@@ -1,16 +1,27 @@
 // for when we have real modules
 import { RenderManager } from './mapwork.rendermanager';
+import { Camera } from './mapwork.view.camera';
+import { Map } from './mapwork.model.map';
 import { isIterable } from 'core-js';
 let testRenderManager;
 let MockEditorEnvironment = {};
 describe('mapwork.rendermanager.js', () => {
   beforeEach(() => {
     testRenderManager = new RenderManager(MockEditorEnvironment);
+    testRenderManager.mapModel = new Map({});
+    testRenderManager.mapModel.createBlankModel('test', 32, 32, 8, 6);
+    testRenderManager.camera = new Camera(testRenderManager.mapModel);
+    testRenderManager.camera.setupCamera(0, 0, 128, 128, 1024, 768);
+
+    document.body.innerHTML = `<div>
+      <canvas id="editorCanvas"></canvas>
+    </div>
+    `;
   });
   describe('constructor', () => {
     it(`should instantiate RenderManager with some sensible defaults`, () => {
-      expect(testRenderManager.mapModel).toEqual(null);
-      expect(testRenderManager.camera).toEqual(null);
+      // expect(testRenderManager.mapModel).toEqual(null);
+      // expect(testRenderManager.camera).toEqual(null);
       expect(testRenderManager.renderFlag).toBe(true);
       expect(testRenderManager.viewFPS).toEqual(20);
       expect(testRenderManager.tilesetTilesAccross).toEqual(null);
@@ -38,7 +49,59 @@ describe('mapwork.rendermanager.js', () => {
       expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 50);
     });
   });
-  describe('DrawMap()', () => {});
+  describe('DrawMap()', () => {
+    jest.clearAllMocks();
+    let clearCanvasSpy,
+      renderMapTilesSpy,
+      renderAreaSelectToolSpy,
+      drawStencilBrushSpy,
+      renderGridSpy,
+      renderTilePickerSpy;
+
+    beforeEach(() => {
+      clearCanvasSpy = jest.spyOn(testRenderManager, 'clearCanvas');
+      renderMapTilesSpy = jest.spyOn(testRenderManager, 'renderMapTiles');
+      renderAreaSelectToolSpy = jest.spyOn(
+        testRenderManager,
+        'renderAreaSelectTool'
+      );
+      drawStencilBrushSpy = jest.spyOn(testRenderManager, 'drawStencilBrush');
+      renderTilePickerSpy = jest.spyOn(testRenderManager, 'renderTilePicker');
+      renderGridSpy = jest.spyOn(testRenderManager, 'renderGrid');
+    });
+    it(`should run all rendering routines, as we have a map model and render flag is set to true`, () => {
+      testRenderManager.renderFlag = true;
+      testRenderManager.DrawMap();
+
+      expect(clearCanvasSpy).toHaveBeenCalledTimes(1);
+      expect(renderMapTilesSpy).toHaveBeenCalledTimes(1);
+      expect(renderAreaSelectToolSpy).toHaveBeenCalledTimes(1);
+      expect(drawStencilBrushSpy).toHaveBeenCalledTimes(1);
+      expect(renderTilePickerSpy).toHaveBeenCalledTimes(1);
+      expect(renderGridSpy).toHaveBeenCalledTimes(1);
+    });
+    it(`should not run any rendering routines, render flag is set to false`, () => {
+      testRenderManager.renderFlag = false;
+      testRenderManager.DrawMap();
+
+      expect(clearCanvasSpy).not.toHaveBeenCalled();
+      expect(renderMapTilesSpy).not.toHaveBeenCalled();
+      expect(renderAreaSelectToolSpy).not.toHaveBeenCalled();
+      expect(drawStencilBrushSpy).not.toHaveBeenCalled();
+      expect(renderTilePickerSpy).not.toHaveBeenCalled();
+    });
+    it(`should not run any rendering routines, we have no map model`, () => {
+      testRenderManager.renderFlag = true;
+      testRenderManager.mapModel = null;
+      testRenderManager.DrawMap();
+
+      expect(clearCanvasSpy).not.toHaveBeenCalled();
+      expect(renderMapTilesSpy).not.toHaveBeenCalled();
+      expect(renderAreaSelectToolSpy).not.toHaveBeenCalled();
+      expect(drawStencilBrushSpy).not.toHaveBeenCalled();
+      expect(renderTilePickerSpy).not.toHaveBeenCalled();
+    });
+  });
   describe('clearCanvas(context, width, height)', () => {});
   describe('drawStencilBrush(context)', () => {});
   describe('renderMapTiles(context)', () => {});
