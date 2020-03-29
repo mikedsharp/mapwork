5b0230bd75266df2e5d9942c22da50c656395f9f
programmerinprogress <msharp23@hotmail.co.uk>
2020-03-29 19:56:27 +0100

    experimenting with validation functions the Svelte way.

--- Scripts/CreateProjectWizard/NameMapStep.svelte
+++ Scripts/CreateProjectWizard/NameMapStep.svelte
@@ -1,23 +1,26 @@
 <script>
   import Dialog from '../Dialog/Dialog'
   import { setupStage } from '../Stores/CreateProjectWizard'
-
+  let mapName = 'untitled map 1'
   function onCancelCreateProject() {
     setupStage.set('')
   }
   function onConfirmCreateProject() {
     setupStage.set('set-map-dimensions')
   }
+  $: valid = mapName && mapName.length > 0
 </script>
 
 <Dialog {$setupStage} dialogWidth="500px" dialogHeight="500px">
   <h1 slot="dialog-title">New Map</h1>
   <div slot="dialog-content">
     <span>Enter a name for your new map</span>
-    <input type="text" />
+    <input bind:value={mapName} type="text" />
   </div>
   <div slot="dialog-actions">
-    <button type="button" on:click={onConfirmCreateProject}>Next</button>
+    <button type="button" disabled={!valid} on:click={onConfirmCreateProject}>
+      Next
+    </button>
     <button type="button" on:click={onCancelCreateProject}>Cancel</button>
   </div>
 </Dialog>
--- Scripts/CreateProjectWizard/SetMapDimensionsStep.svelte
+++ Scripts/CreateProjectWizard/SetMapDimensionsStep.svelte
@@ -1,6 +1,8 @@
 <script>
   import Dialog from '../Dialog/Dialog'
   import { setupStage } from '../Stores/CreateProjectWizard'
+  let tilesAccross = 32
+  let tilesDown = 32
 
   function onCancelCreateProject() {
     setupStage.set('')
@@ -8,18 +10,24 @@
   function onConfirmCreateProject() {
     setupStage.set('')
   }
+  function isInvalid() {
+    return true
+  }
+  $: valid = tilesAccross > 0 && tilesDown > 0
 </script>
 
 <Dialog {$setupStage} dialogWidth="500px" dialogHeight="500px">
   <h1 slot="dialog-title">New Map</h1>
   <div slot="dialog-content">
     <span>Number of horizontal tiles</span>
-    <input type="number" />
+    <input type="number" bind:value={tilesAccross} />
     <span>Number of vertical tiles</span>
-    <input type="number" />
+    <input type="number" bind:value={tilesDown} />
   </div>
   <div slot="dialog-actions">
-    <button type="button" on:click={onConfirmCreateProject}>Ok</button>
+    <button disabled={!valid} type="button" on:click={onConfirmCreateProject}>
+      Ok
+    </button>
     <button type="button" on:click={onCancelCreateProject}>Cancel</button>
   </div>
 </Dialog>
